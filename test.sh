0686aa32ef4a1f69c589e52a5fcd667f7149adf2
Narendra Ingale <narendra.ingale2@gmail.com>
2017-06-17 08:56:38 +0000

    Added test script

---
+++ test.sh
@@ -0,0 +1,11 @@
+sudo yum install docker git nodejs npm  mongodb-org -y
+sudo systemctl start docker 
+sudo systemctl start mongod
+npm install
+node server.js >> file.txt 2>&1 &
+sleep 5
+wget -O - http://localhost:8080 
+exit_status=$?
+ps -ef|grep -i server.js|grep -v grep|awk '{ print $2 }'|xargs kill -9
+rm file.txt
+exit $exit_status
